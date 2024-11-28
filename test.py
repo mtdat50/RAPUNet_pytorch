b4cddf9c7e893cf2db1c96238a7706254aa9dc07
mtdat50 <maitandat502@gmail.com>
2024-11-28 19:05:40 +0700

    Modify loss calculation in test function to be more accurate.

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ def test(model, test_loader, loss_fn, device):
     model.to(device)
 
     total_loss = 0.0
+    sample_count = 0
     for batch_count, (batch_input, batch_gt) in enumerate(test_loader):
         batch_input = batch_input.to(device)
         batch_gt = batch_gt.to(device)
@@ -22,11 +23,12 @@ def test(model, test_loader, loss_fn, device):
             batch_pred = model(batch_input)
             batch_loss = loss_fn(batch_pred, batch_gt)
 
-        total_loss += batch_loss.item()
-        print(f"Validating ({batch_count + 1}/{len(test_loader)}): Loss {total_loss / (batch_count + 1)}" + ' '*50, end='\r')
+        total_loss += batch_loss.item() * batch_input.shape[0]
+        sample_count += batch_input.shape[0]
+        print(f"Testing ({batch_count + 1}/{len(test_loader)}): Loss {total_loss / sample_count}" + ' '*50, end='\r')
     print('\n')
 
-    return total_loss / len(test_loader)
+    return total_loss / sample_count
 
 
 aug_test = albu.Compose([
@@ -39,22 +41,24 @@ if __name__ == "__main__":
     print('Running on device: {}'.format(device))
 
     batch_size = 16
-    starting_kernels = 16
-
-    datasets = ["CVC-300", "CVC-ClinicDB", "CVC-ColonDB", "ETIS-LaribPolypDB", "Kvasir"]
-    test_path = f"../data/TestDataset/{datasets[3]}/"
-
-    X, Y = glob_paths(test_path)
-    test_set = FolderDataset(X, Y, transform=aug_test)
-    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=4)
-
-
-    model_path = "/kaggle/working/.........."
-    model = RAPUNet(in_channels=3, out_classes=1, starting_kernels=starting_kernels)
+    
+    model_path = "/kaggle/working/bestmodel_epoch93_loss0.12060173153877259.pt"
+    model = RAPUNet(in_channels=3, out_classes=1)
     model.load_state_dict(torch.load(model_path, weights_only=True))
     
     loss_fn = dice_loss
 
-    test_loss = test(model, test_loader, loss_fn, device)
-    with open("/kaggle/working/test_log.txt", "a") as f:
-        f.write(f"Validating: Loss {test_loss / len(test_loader)}\n\n")
+    
+    datasets = ["CVC-300", "CVC-ClinicDB", "CVC-ColonDB", "ETIS-LaribPolypDB", "Kvasir"]
+    for dataset in datasets:
+        print(f"{dataset}:")
+        test_path = f"/kaggle/input/polyp-dataset/data/TestDataset/{dataset}/"
+    
+        X, Y = glob_paths(test_path)
+        test_set = FolderDataset(X, Y, transform=aug_test)
+        test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=4)
+    
+        test_loss = test(model, test_loader, loss_fn, device)
+        with open(f"/kaggle/working/test_log.txt", "a") as f:
+            f.write(f"{dataset}: Loss {test_loss}\n")
+            f.write(f"{dataset}: mDice {1 - test_loss}\n\n")
