f7af7229078d69ef14599621fddeca474b2f70b2
Đạt <62801450+mtdat50@users.noreply.github.com>
2025-11-20 18:22:22 +0700

    figured out silly typo that killed the model's performance lol
    pretrained arg in timm.create_model.
    the code failed silently due to **kwargs

--- ModelArchitecture/RAPUNet.py
+++ ModelArchitecture/RAPUNet.py
@@ -10,7 +10,7 @@ interpolation = "nearest"
 class RAPUNet(nn.Module):
     def __init__(self, in_channels, out_classes, starting_kernels = 16):
         super().__init__()
-        self.backbone = timm.create_model('caformer_s18.sail_in22k_ft_in1k_384', pretraned=True, features_only=True)
+        self.backbone = timm.create_model('caformer_s18.sail_in22k_ft_in1k_384', pretrained=True, features_only=True)
 
 
         self.p1_conv2d = nn.Conv2d(in_channels, starting_kernels * 2, 3, stride=2)
